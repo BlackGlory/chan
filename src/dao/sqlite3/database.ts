a4f2973ec69b74902c4f82a431af89504b1114ef
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-10-18 23:52:38 +0800

    feat: ensure data dir

--- src/dao/sqlite3/database.ts
+++ src/dao/sqlite3/database.ts
@@ -1,8 +1,12 @@
 import Database = require('better-sqlite3')
 import { path as appRoot } from 'app-root-path'
 import * as path from 'path'
+import * as fs from 'fs-extra'
 
-let db = new Database(path.join(appRoot, 'data/database.sqlite'))
+const dataDir = path.join(appRoot, 'data')
+const dataFile = path.join(dataDir, 'database.sqlite')
+fs.ensureDirSync(dataDir)
+let db = new Database(dataFile)
 
 export function getDatabase() {
   if (!db.open) reconnectDatabase()
@@ -11,5 +15,5 @@ export function getDatabase() {
 
 export function reconnectDatabase() {
   db.close()
-  db = new Database(path.join(appRoot, 'data/database.sqlite'))
+  db = new Database(dataFile)
 }
