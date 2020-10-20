e805cd6f33aa2845f68ebc22d0f455a810651e75
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-10-20 13:53:51 +0800

    refactor: split api.spec.ts

---
+++ __tests__/services/api/token-based-access-control.spec.ts
@@ -0,0 +1,332 @@
+import { buildServer } from '@src/server'
+import { prepareDatabase, resetEnvironment } from '@test/utils'
+import { matchers } from 'jest-json-schema'
+import { tokenSchema } from '@src/schema'
+
+jest.mock('@dao/sqlite3/database')
+expect.extend(matchers)
+
+beforeEach(async () => {
+  resetEnvironment()
+  await prepareDatabase()
+})
+
+describe('TBAC', () => {
+  describe('GET /api/mpmc-with-tokens', () => {
+    describe('auth', () => {
+      it('200', async () => {
+        process.env.MPMC_ADMIN_PASSWORD = 'password'
+        const server = buildServer()
+
+        const res = await server.inject({
+          method: 'GET'
+        , url: '/api/mpmc-with-tokens'
+        , headers: createAuthHeaders()
+        })
+
+        expect(res.statusCode).toBe(200)
+        expect(res.json()).toMatchSchema({
+          type: 'array'
+        , items: { type: 'string' }
+        })
+      })
+    })
+
+    describe('no admin password', () => {
+      it('401', async () => {
+        const server = buildServer()
+
+        const res = await server.inject({
+          method: 'GET'
+        , url: '/api/mpmc-with-tokens'
+        })
+
+        expect(res.statusCode).toBe(401)
+      })
+    })
+
+    describe('bad auth', () => {
+      it('401', async () => {
+        process.env.MPMC_ADMIN_PASSWORD = 'password'
+        const server = buildServer()
+
+        const res = await server.inject({
+          method: 'GET'
+        , url: '/api/mpmc-with-tokens'
+        , headers: createAuthHeaders('bad')
+        })
+
+        expect(res.statusCode).toBe(401)
+      })
+    })
+  })
+
+  describe('GET /api/mpmc/:id/tokens', () => {
+    describe('auth', () => {
+      it('200', async () => {
+        process.env.MPMC_ADMIN_PASSWORD = 'password'
+        const server = buildServer()
+        const id = 'id'
+
+        const res = await server.inject({
+          method: 'GET'
+        , url: `/api/mpmc/${id}/tokens`
+        , headers: createAuthHeaders()
+        })
+
+        expect(res.statusCode).toBe(200)
+        expect(res.json()).toMatchSchema({
+          type: 'array'
+        , items: {
+            type: 'object'
+          , properties: {
+              token: tokenSchema
+            , enqueue: { type: 'boolean' }
+            , dequeue: { type: 'boolean' }
+            }
+          }
+        })
+      })
+    })
+
+    describe('no admin password', () => {
+      it('401', async () => {
+        const server = buildServer()
+        const id = 'id'
+
+        const res = await server.inject({
+          method: 'GET'
+        , url: `/api/mpmc/${id}/tokens`
+        })
+
+        expect(res.statusCode).toBe(401)
+      })
+    })
+
+    describe('bad auth', () => {
+      it('401', async () => {
+        process.env.MPMC_ADMIN_PASSWORD = 'password'
+        const server = buildServer()
+        const id = 'id'
+
+        const res = await server.inject({
+          method: 'GET'
+        , url: `/api/mpmc/${id}/tokens`
+        , headers: createAuthHeaders('bad')
+        })
+
+        expect(res.statusCode).toBe(401)
+      })
+    })
+  })
+
+  describe('PUT /api/mpmc/:id/tokens/:token/enqueue', () => {
+    describe('auth', () => {
+      it('204', async () => {
+        process.env.MPMC_ADMIN_PASSWORD = 'password'
+        const server = buildServer()
+        const id = 'id'
+        const token = 'token'
+
+        const res = await server.inject({
+          method: 'PUT'
+        , url: `/api/mpmc/${id}/tokens/${token}/enqueue`
+        , headers: createAuthHeaders()
+        })
+
+        expect(res.statusCode).toBe(204)
+      })
+    })
+
+    describe('no admin password', () => {
+      it('401', async () => {
+        const server = buildServer()
+        const id = 'id'
+        const token = 'token'
+
+        const res = await server.inject({
+          method: 'PUT'
+        , url: `/api/mpmc/${id}/tokens/${token}/enqueue`
+        })
+
+        expect(res.statusCode).toBe(401)
+      })
+    })
+
+    describe('bad auth', () => {
+      it('401', async () => {
+        process.env.MPMC_ADMIN_PASSWORD = 'password'
+        const server = buildServer()
+        const id = 'id'
+        const token = 'token'
+
+        const res = await server.inject({
+          method: 'PUT'
+        , url: `/api/mpmc/${id}/tokens/${token}/enqueue`
+        , headers: createAuthHeaders('bad')
+        })
+
+        expect(res.statusCode).toBe(401)
+      })
+    })
+  })
+
+  describe('DELETE /api/mpmc/:id/tokens/:token/enqueue', () => {
+    describe('auth', () => {
+      it('204', async () => {
+        process.env.MPMC_ADMIN_PASSWORD = 'password'
+        const server = buildServer()
+        const id = 'id'
+        const token = 'token'
+
+        const res = await server.inject({
+          method: 'DELETE'
+        , url: `/api/mpmc/${id}/tokens/${token}/enqueue`
+        , headers: createAuthHeaders()
+        })
+
+        expect(res.statusCode).toBe(204)
+      })
+    })
+
+    describe('no admin password', () => {
+      it('401', async () => {
+        const server = buildServer()
+        const id = 'id'
+        const token = 'token'
+
+        const res = await server.inject({
+          method: 'DELETE'
+        , url: `/api/mpmc/${id}/tokens/${token}/enqueue`
+        })
+
+        expect(res.statusCode).toBe(401)
+      })
+    })
+
+    describe('bad auth', () => {
+      it('401', async () => {
+        process.env.MPMC_ADMIN_PASSWORD = 'password'
+        const server = buildServer()
+        const id = 'id'
+        const token = 'token'
+
+        const res = await server.inject({
+          method: 'DELETE'
+        , url: `/api/mpmc/${id}/tokens/${token}/enqueue`
+        , headers: createAuthHeaders('bad')
+        })
+
+        expect(res.statusCode).toBe(401)
+      })
+    })
+  })
+
+  describe('PUT /api/mpmc/:id/tokens/:token/dequeue', () => {
+    describe('auth', () => {
+      it('204', async () => {
+        process.env.MPMC_ADMIN_PASSWORD = 'password'
+        const server = buildServer()
+        const id = 'id'
+        const token = 'token'
+
+        const res = await server.inject({
+          method: 'PUT'
+        , url: `/api/mpmc/${id}/tokens/${token}/dequeue`
+        , headers: createAuthHeaders()
+        })
+
+        expect(res.statusCode).toBe(204)
+      })
+    })
+
+    describe('no admin password', () => {
+      it('401', async () => {
+        const server = buildServer()
+        const id = 'id'
+        const token = 'token'
+
+        const res = await server.inject({
+          method: 'PUT'
+        , url: `/api/mpmc/${id}/tokens/${token}/dequeue`
+        })
+
+        expect(res.statusCode).toBe(401)
+      })
+    })
+
+    describe('bad auth', () => {
+      it('401', async () => {
+        process.env.MPMC_ADMIN_PASSWORD = 'password'
+        const server = buildServer()
+        const id = 'id'
+        const token = 'token'
+
+        const res = await server.inject({
+          method: 'PUT'
+        , url: `/api/mpmc/${id}/tokens/${token}/dequeue`
+        , headers: createAuthHeaders('bad')
+        })
+
+        expect(res.statusCode).toBe(401)
+      })
+    })
+  })
+
+  describe('DELETE /api/mpmc/:id/tokens/:token/dequeue', () => {
+    describe('auth', () => {
+      it('204', async () => {
+        process.env.MPMC_ADMIN_PASSWORD = 'password'
+        const server = buildServer()
+        const id = 'id'
+        const token = 'token'
+
+        const res = await server.inject({
+          method: 'DELETE'
+        , url: `/api/mpmc/${id}/tokens/${token}/dequeue`
+        , headers: createAuthHeaders()
+        })
+
+        expect(res.statusCode).toBe(204)
+      })
+    })
+
+    describe('no admin password', () => {
+      it('401', async () => {
+        const server = buildServer()
+        const id = 'id'
+        const token = 'token'
+
+        const res = await server.inject({
+          method: 'DELETE'
+        , url: `/api/mpmc/${id}/tokens/${token}/dequeue`
+        })
+
+        expect(res.statusCode).toBe(401)
+      })
+    })
+
+    describe('bad auth', () => {
+      it('401', async () => {
+        process.env.MPMC_ADMIN_PASSWORD = 'password'
+        const server = buildServer()
+        const id = 'id'
+        const token = 'token'
+
+        const res = await server.inject({
+          method: 'DELETE'
+        , url: `/api/mpmc/${id}/tokens/${token}/dequeue`
+        , headers: createAuthHeaders('bad')
+        })
+
+        expect(res.statusCode).toBe(401)
+      })
+    })
+  })
+})
+
+function createAuthHeaders(adminPassword?: string) {
+  return {
+    'Authorization': `Bearer ${ adminPassword ?? process.env.MPMC_ADMIN_PASSWORD }`
+  }
+}
