9342fdf43d81464bba2a307b75c0518a0c73f604
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-10-27 00:09:29 +0800

    chore: update comments

--- migrations/config/001-initial.sql
+++ migrations/config/001-initial.sql
@@ -6,7 +6,7 @@
 PRAGMA journal_mode = WAL;
 
 -- SQLite 会将VARCHAR(255)转换为TEXT, 将BOOLEAN转换为NUMERIC, 使用这些数据类型是出于可读性考虑
--- mpmc资源本身是松散的, 无需专门创建表
+-- mpmc资源本身是松散的, 没有自己的表
 
 CREATE TABLE mpmc_json_schema (
   mpmc_id     VARCHAR(255) NOT NULL UNIQUE
