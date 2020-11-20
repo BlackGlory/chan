08f0823218600e4ad8a5ad5a407c44160f2f7982
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-11-20 11:22:33 +0800

    chore: remove async

--- __tests__/utils.ts
+++ __tests__/utils.ts
@@ -29,7 +29,7 @@ export async function prepareJsonSchemaDatabase() {
   return db
 }
 
-export async function resetEnvironment() {
+export function resetEnvironment() {
   // assigning a property on `process.env` will implicitly convert the value to a string.
   // use `delete` to delete a property from `process.env`.
   // sjee also: https://nodejs.org/api/process.html#process_process_env
