a9f705e82a32b990f7d95b43e882d3983e5b21eb
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-12-18 01:27:53 +0800

    feat: add robots.txt

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import cors from 'fastify-cors'
 import { routes as chan } from '@services/chan'
 import { routes as api } from '@services/api'
 import { routes as stats } from '@services/stats'
+import { routes as robots } from '@services/robots'
 import { HTTP2, PAYLOAD_LIMIT, NODE_ENV, NodeEnv } from '@env'
 import { Core } from '@core'
 
@@ -23,6 +24,7 @@ export async function buildServer() {
   server.register(chan, { Core })
   server.register(api, { Core })
   server.register(stats, { Core })
+  server.register(robots)
   return server
 }
 
