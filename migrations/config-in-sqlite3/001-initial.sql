2bfd2369e8ff1157bc63a7732115e3bc390d0681
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2020-12-18 16:37:55 +0800

    feat: remove indexes

--- migrations/config-in-sqlite3/001-initial.sql
+++ migrations/config-in-sqlite3/001-initial.sql
@@ -12,24 +12,15 @@ CREATE TABLE chan_blacklist (
   chan_id VARCHAR(255) NOT NULL UNIQUE
 );
 
-CREATE UNIQUE INDEX idx_chan_blacklist_chan_id
-    ON chan_blacklist(chan_id);
-
 CREATE TABLE chan_whitelist (
   chan_id VARCHAR(255) NOT NULL UNIQUE
 );
-
-CREATE UNIQUE INDEX idx_chan_whitelist_chan_id
-    ON chan_whitelist(chan_id);
 
 CREATE TABLE chan_token_policy (
   chan_id              VARCHAR(255) NOT NULL UNIQUE
 , write_token_required BOOLEAN
 , read_token_required  BOOLEAN
 );
-
-CREATE UNIQUE INDEX idx_chan_token_policy_chan_id
-    ON chan_token_policy(chan_id);
 
 CREATE TABLE chan_token (
   chan_id          VARCHAR(255) NOT NULL
@@ -39,16 +30,10 @@ CREATE TABLE chan_token (
 , UNIQUE (token, chan_id)
 );
 
-CREATE UNIQUE INDEX idx_chan_token_policy_chan_id_token
-    ON chan_token(chan_id,token);
-
 CREATE TABLE chan_json_schema (
   chan_id     VARCHAR(255) NOT NULL UNIQUE
 , json_schema TEXT         NOT NULL
 );
-
-CREATE UNIQUE INDEX idx_chan_json_schema_chan_id
-    ON chan_json_schema(chan_id);
 
 --------------------------------------------------------------------------------
 -- Down
